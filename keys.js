be6d7870c6f1a562efb1ea42e37faaeeca6ae12b
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-04-16 22:45:21 -0600

    append file

--- keys.js
+++ keys.js
@@ -1,10 +1,6 @@
 console.log( "this is loaded" );
 exports.spotify = {
-    id: process.env.f9f76d0c27d413d8b3f66f01e9e3e80,
-    secret: process.env.ce84b1418c4a228e84a0c660e60985
+    id: 3f9f76d0c27d413d8b3f66f01e9e3e80,
+
+    secret: ce84b1418c4a228e84a0c660e60985
 };
-// console.log( "this is loaded" );
-// exports.spotify = {
-//     id: process.env.553f5c8e2ae144ef9f6fa43288e246a1,
-//     secret: process.env.d776038517124cfdbf531fc55b205035
-// };
--- liri.js
+++ liri.js
@@ -2,8 +2,9 @@ var fs = require( "fs" );
 // var inquirer = require( "inquirer" );
 var axios = require( "axios" )
 var moment = require( "moment" )
+moment().format();
 var Spotify = require( 'node-spotify-api' );
-var Keys = require( "./keys" );
+// var Keys = require( "./keys" );
 
 var choice = process.argv[2];
 var info = process.argv.slice( 3 ).join( " " );
@@ -24,6 +25,13 @@ switch ( choice ) {
 }
 
 function Movie() {
+
+
+
+    if ( info == undefined ) {
+        info = "Mr. Nobody"
+    }
+
     var queryUrl = "http://www.omdbapi.com/?t=&y=&plot=short&apikey=trilogy";
     console.log( queryUrl );
 
@@ -36,27 +44,58 @@ function Movie() {
             console.log( "Release: " + response.data.Year );
             console.log( "IMDB rating: " + response.data.imdbRating );
             //    * Rotten Tomatoes Rating of the movie.
-            // console.log( "Rotten Tomatoes rating: " + response.data.Ratings[1].Value );
+            console.log( "Rotten Tomatoes rating: " + response.data.Ratings[1].Value );
             console.log( "Country origin: " + response.data.Country );
             console.log( "Language: " + response.data.Language );
             console.log( "Plot: " + response.data.Plot );
             console.log( "Actors: " + response.data.Actors );
         } );
+    fs.appendFile( "log.txt", info, function ( err ) {
 
-};
+        // If an error was experienced we will log it.
+        if ( err ) {
+            console.log( err );
+        }
+
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+            console.log( "Content Added!" );
+        }
+    } );
+}
+
 
 function Bands() {
+    if ( info == undefined ) {
+        info = "The Killers";
+    }
+
     var queryUrl = "https://rest.bandsintown.com/artists//events?app_id=codingbootcamp";
     console.log( queryUrl );
     axios.get( "https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp" ).then(
         function ( response ) {
             for ( var i = 0; i < 6; i++ ) {
+                moment().format();
                 console.log( "Name of the venue: " + response.data[i].venue.name )
                 console.log( "Venue location: " + response.data[i].venue.city )
                 console.log( "Venue date: " + response.data[i].datetime )
 
+
             }
         } )
+}
+
+
+
+
+function Music() {
+    var queryUrl = "https://api.spotify.com/v1/tracks"
+    console.log( queryUrl );
+    axios.get( "https://api.spotify.com/v1/tracks" + info ).then(
+        function ( response ) {
+            console.log( "Artist: " + response.data.artist )
+        }
+    )
 }
 
 
